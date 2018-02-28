f5f678a42da71e6c60280408b813421ec0cac28d
driedl <david.riedl@elements.at>
2018-02-28 13:58:14 +0100

    - removed unnecessary admin css file ~ fixed composer bundle class

--- src/RestBundle/DavesWeblabRestBundle.php
+++ src/RestBundle/DavesWeblabRestBundle.php
@@ -6,9 +6,4 @@ use Pimcore\Extension\Bundle\AbstractPimcoreBundle;
 
 class DavesWeblabRestBundle extends AbstractPimcoreBundle
 {
-	public function getCssPaths() {
-		return [
-			"/assets/css/daves-weblab-theme.css"
-		];
-	}
 }
