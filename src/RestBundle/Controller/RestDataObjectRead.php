0e7575b9913e94051c59a1f9d80662837e278869
Michael Cuénez <mcuenez@gmail.com>
2018-05-14 20:09:58 +0200

    + Added exception ~ Updated object reader to use MyModel::class rather than a plain string

--- src/RestBundle/Controller/RestDataObjectRead.php
+++ src/RestBundle/Controller/RestDataObjectRead.php
@@ -8,15 +8,21 @@ use DavesWeblab\RestBundle\Rest\DataObject\Read\ListPaginationFilter;
 use DavesWeblab\RestBundle\Serializer\Serializer;
 use Pimcore\Model\DataObject\ClassDefinition;
 use Pimcore\Model\Listing\AbstractListing;
+use DavesWeblab\RestBundle\Exception\InvalidNamespaceException;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Pimcore\Model\DataObject;
+use Pimcore\Model\DataObject\AbstractObject;
 
 trait RestDataObjectRead
 {
     /**
-     * @return string
+     * To avoid any InvalidNamespaceException's, create a new model class (e.g. MyModel) and extend from
+     * the AbstractObject class. Afterwards, return MyModel::class.
+     *
+     * @return string the class (including its namespace) that is to be used as model.
      */
     abstract protected function getClassName();
 
@@ -24,13 +30,17 @@ trait RestDataObjectRead
     {
     }
 
+    /**
+     * Alias for this#getClassName().
+     * @return string
+     */
     protected function getClass()
     {
-        return "\\Pimcore\\Model\\DataObject\\{$this->getClassName()}";
+        return $this->getClassName();
     }
 
     /**
-     * @return ClassDefinition
+     * @return ClassDefinition|null null is returned if an error occurred.
      */
     protected function getClassDefinition()
     {
@@ -70,22 +80,12 @@ trait RestDataObjectRead
      */
     public function listAction(Serializer $serializer, Request $request)
     {
+        $this->assertClass();
+
+        $listClass = $this->getClassName();
+        $list = $listClass::getList();
+
         $filters = $this->getListFilters();
-
-        $listClass = "\\Pimcore\\Model\\DataObject\\{$this->getClassName()}";
-
-        if (!class_exists($listClass)) {
-            throw new \InvalidArgumentException("Invalid class name given. {$listClass}");
-        }
-
-        try {
-            /**
-             * @var AbstractListing $list
-             */
-            $list = $listClass::getList();
-        } catch (\Exception $e) {
-            throw new \InvalidArgumentException("Invalid object class given. {$listClass}::getList");
-        }
 
         foreach ($filters as $filter) {
             $filter->apply($request, $list);
@@ -100,8 +100,9 @@ trait RestDataObjectRead
      */
     public function getAction($id, Serializer $serializer, Request $request)
     {
-        $className = $this->getClass();
+        $this->assertClass();
 
+        $className = $this->getClass();
         try {
             $object = $className::getById($id);
 
@@ -127,5 +128,50 @@ trait RestDataObjectRead
     public function optionsAction()
     {
         return new Response("OK");
+    }
+
+    /**
+     * @return string the valid namespace. This will be used to check against the given classes namespace.
+     */
+    private function getValidClass()
+    {
+        return AbstractObject::class;
+    }
+
+    /**
+     * Asserts the class that was set by the user.
+     */
+    private function assertClass()
+    {
+        $className = $this->getClass();
+
+        $this->assertClassExistence($className);
+        $this->assertNamespace($className);
+    }
+
+    /**
+     * Asserts the existence of the class name.
+     *
+     * @param string $className the class name that was set by the user
+     * @throws InvalidNamespaceException if the class does not exist.
+     */
+    private function assertClassExistence($className)
+    {
+        if (!class_exists($className)) {
+            throw new InvalidNamespaceException($this->getValidClass());
+        }
+    }
+
+    /**
+     * Asserts the namepsace of the class name. Call this#assertClassExistence() beforehand, to ensure the class exists.
+     *
+     * @param string $className the class name that was set by the user
+     * @throws InvalidNamespaceException if the class is not within a valid namespace.
+     */
+    private function assertNamespace($className)
+    {
+        if(!is_a($className, $this->getValidClass(), true)){
+            throw new InvalidNamespaceException($this->getValidClass());
+        }
     }
 }
